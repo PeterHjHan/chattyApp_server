9ad2b4bf5791b86cafbb94ede0c0692d9f392f12
= <peterhjhan@gmail.com>
2018-11-03 14:19:21 +0000

    create README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chatty Server App
+
+This is the required server side files to [Chatty application](https://github.com/PeterHjHan/chattyApp)
+
+### Dependencies
+* ws
+* express
+* uuid
+
+
+### Getting Started
+1. Clone both the Main Chatty application and this server repo
+2. Start the server first by typing `npm start` 
+3. Port is set default to 3001
--- server.js
+++ server.js
@@ -1,48 +1,58 @@
-// server.js
 const express = require('express');
 const SocketServer = require('ws').Server;
 const uuid = require('uuid');
 
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-   // Must change directory if not seen
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
 const wss = new SocketServer({ server });
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+
+function updateUserCount() {
+  const userCount = {
+   type: "userCount",
+   userCount: wss.clients.size,
+  }
+  wss.clients.forEach(function each(client) {
+   client.send(JSON.stringify(userCount))
+ })
+}
+
 wss.on('connection', (ws) => {
   console.log(`${uuid()}: Client connected `);
-
+  updateUserCount();
+  
   ws.onmessage = (event) => {
     const messageData = JSON.parse(event.data);
     const username = messageData.user;
     const message = messageData.message;
+    const type = messageData.type;
 
     var data = {
       id: uuid(),
+      type: type,
       username: username,
-      message : message
+      content : message,
     }
 
+    switch(data.type){
+      case "incomingNotification":
+        data.type = "postNotification";
+      break;
+      case "incomingChat":
+        data.type = "postChat";
+      break;
+    }
     wss.clients.forEach(function each(client) {
-      if (client !== ws) {
         client.send(JSON.stringify(data))
-        console.log("THIS IS THE MESSAGE", JSON.stringify(data))
-      }
     })
-
-    // console.log(`User ${username} says ${message}`)
   }
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    updateUserCount()
+    console.log('Client disconnected');
+  })
 });
